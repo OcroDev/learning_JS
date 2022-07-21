a4238a48a8587786c51ae63bcdb27b4af54e231a
Rohermy Ochoa <rohermy.ochoa@gmail.com>
2022-07-21 16:57:23 -0400

    inicio de CRUD en API Rest

---
+++ CRUD_AJAX/crud.js
@@ -0,0 +1,6 @@
+const d = document,
+  $table = d.querySelector(".crud-table"),
+  $form = d.querySelector(".crud-form"),
+  $tittle = d.querySelector(".crud-tittle"),
+  $template = d.querySelector(".crud-template").content,
+  $fragment = d.createDocumentFragment();
--- ajax.js
+++ ajax.js
@@ -12,7 +12,7 @@
     }
     //validacion para verificar la respuesta del servidor
     if (xhr.status >= 200 && xhr.status < 300) {
-      console.log("exito");
+      // console.log("exito");
       //console.log(xhr.responseText);
       let json = JSON.parse(xhr.responseText);
       json.forEach((element) => {
@@ -26,12 +26,10 @@
       let message = xhr.statusText || "Ocurrio un error";
       $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
     }
-
     //console.log(xhr);
   });
 
   xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
-
   xhr.send();
 })();
 
@@ -59,10 +57,12 @@
       let message = err.statusText || "Ocurrio un error";
       $fetch.innerHTML = `Error ${err.status}: ${message}`;
     })
-    .finally(console.log("Codigo ejecutado independiente de la respuesta"));
+    .finally
+    //console.log("Codigo ejecutado independiente de la respuesta fetch normal")
+    ();
 })();
 
-/*********************** 108 AJAX: API Fetch + Async+Await ****************************/
+/********************* 108 AJAX: API Fetch + Async+Await ***********************/
 
 (() => {
   const $fetchAsync = document.getElementById("fetch-async"),
@@ -72,9 +72,6 @@
     try {
       let res = await fetch("https://jsonplaceholder.typicode.com/users"),
         json = await res.json();
-
-      console.log(res, json);
-
       if (!res.ok) {
         throw { status: res.status, statusText: res.statusText };
       }
@@ -87,11 +84,65 @@
 
       $fetchAsync.appendChild($fragment);
     } catch (err) {
-      console.log(err);
       let message = err.statusText || "Ocurrio un error";
       $fetchAsync.innerHTML = `Error ${err.status}: ${message}`;
     } finally {
-      console.log("finally await");
+      //   console.log("finally await");
+    }
+  }
+  getData();
+})();
+
+/********************* 109 AJAX: API Axios ***********************/
+(() => {
+  const $axios = document.getElementById("axios"),
+    $fragment = document.createDocumentFragment();
+
+  axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+      //console.log(res);
+      let json = res.data;
+      json.forEach((element) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `nombre: ${element.name} -- ID: ${element.id} -- Phone: ${element.phone}`;
+        $fragment.appendChild($li);
+      });
+
+      $axios.appendChild($fragment);
+    })
+    .catch((err) => {
+      console.log("estamos en el catch de axios", err.response);
+      let message = err.response.statusText || "Ocurrio un error";
+      $axios.innerHTML = `Error ${err.response.status}: ${message}`;
+    })
+    .finally(() => console.log(""));
+})();
+
+/********************* 110 AJAX: API Axios + Async+Await ***********************/
+
+(() => {
+  const $axiosAsync = document.getElementById("axios-async"),
+    $fragment = document.createDocumentFragment();
+
+  async function getData() {
+    try {
+      let res = await axios.get("https://jsonplaceholder.typicode.com/users"),
+        json = await res.data;
+
+      json.forEach((element) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `nombre: ${element.name} -- ID: ${element.id} -- Phone: ${element.phone}`;
+        $fragment.appendChild($li);
+      });
+
+      $axiosAsync.appendChild($fragment);
+    } catch (err) {
+      // console.log("estamos en el catch de axios", err.response);
+      let message = err.response.statusText || "Ocurrio un error";
+      $axiosAsync.innerHTML = `Error ${err.response.status}: ${message}`;
+    } finally {
+      //  console.log("esto se ejecuta independiente del axios async ");
     }
   }
   getData();
